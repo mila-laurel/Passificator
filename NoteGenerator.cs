6a1ec2a991fc6b22e197987659b1f0a11474fb95
mila-laurel <62745003+mila-laurel@users.noreply.github.com>
2020-10-14 22:25:09 +0300

    The start of NoteGenerator

---
+++ NoteGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+using Passificator.Dto;
+
+namespace Passificator
+{
+    class NoteGenerator
+    {
+        private readonly string path = Path.GetFullPath("template.dot");
+
+        public NoteGenerator(NoteContextDTO noteContextDTO)
+        {
+            Application temple = new Application();
+            temple.Documents.Add(path);
+        }
+
+        public void Generate()
+        {
+
+        }
+
+        public void CreateTable(GuestDto guestDto)
+        {
+
+        }
+    }
+}
