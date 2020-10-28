49f6213a2b0c0f86909d9622e0589d44c843eb20
Мила Лаврова <mila.fourteen@gmail.com>
2020-10-28 17:32:24 +0300

    unnecessary usings removed

--- PassGenerator.cs
+++ PassGenerator.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.Office.Interop.Word;
 using Passificator.Dto;
-using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Passificator
 {
